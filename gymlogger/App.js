3c18b1f079a646f427c664d3370e5a7ddf3db413
sindabad764 <sindabad764@outlook.com>
2023-04-10 21:20:44 -0700

    GymLogger : added custom fonts

--- gymlogger/App.js
+++ gymlogger/App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // makes tags appear in right screen view
+    <SafeAreaView
+    style={{
+      flex:1,
+      justifyContent:'center',
+      backgroundColor:'#fff',
+      padding:20
+    }}>
+        <Text style={{
+          fontSize:30,
+          fontWeight:'bold',
+          color:'#20315f',
+          padding: 10
+        }}>
+          GymLogger
+        </Text>
+
+        <TouchableOpacity 
+            style={{
+              backgroundColor:'#AD40AF',
+              padding:20,
+              width:'90%',
+              flexDirection:'row', 
+              justifyContent:'space-between'
+              }}>
+          <Text style={{fontWeight:'bold', fontSize:18,color:"#fff",fontFamily:'Roboto-BlackItalic'}}>Get Logging</Text>
+          <MaterialIcons name='arrow-forward-ios' size={22} color='#fff'/>
+        </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
