ac1cc66876ed25f55b1b0c1ba40afbd79ac50952
sindabad764 <sindabad764@outlook.com>
2023-04-11 17:10:58 -0700

    Added cardio, weight screen

--- gymlogger/App.js
+++ gymlogger/App.js
@@ -1,139 +1,181 @@
-import React from 'react';
+import React from "react";
 import {
   SafeAreaView,
   View,
   Text,
   TouchableOpacity,
-} from 'react-native';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+  ScrollView,
+} from "react-native";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import SambaShoe from "./assets/SambaShoe.svg";
 import Cardio from "./assets/cardio.svg";
 import Weights from "./assets/weights.svg";
-import CardioLogger from './Screens/CardioScreen';
-
+import CardioLogger from "./Screens/CardioScreen";
+import WeightliftingLogger from "./Screens/WeightsScreen";
 
 const Stack = createNativeStackNavigator();
 
-const App = () =>{
-  return(
+const App = () => {
+  return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Main">
-      <Stack.Screen component={Home} name="Home"/>
-      <Stack.Screen component={Main} name="Main" options={{headerShown:false}}/>
-      <Stack.Screen component={CardioScreen} name="CardioScreen" options={{headerShown:false}}/>
+        <Stack.Screen component={Home} name="Home" />
+        <Stack.Screen
+          component={Main}
+          name="Main"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          component={CardioScreen}
+          name="CardioScreen"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          component={WeightsScreen}
+          name="WeightsScreen"
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-    
-  )
-}
+  );
+};
 
-const Home =({navigation}) => {
-  return(
+const Home = ({ navigation }) => {
+  return (
     <SafeAreaView
-    style={{
-      flex:1,
-      justifyContent:'center',
-      backgroundColor:'#fff',
-        alignItems: 'center', // set alignItems to center
-      padding:20
-    }}>
-
-      <View style={{padding:50}}>
-        <TouchableOpacity 
-          onPress={() => navigation.navigate('CardioScreen')}
-            style={{
-              backgroundColor:'#3EB489',
-              padding:20,
-              width:'70%',
-              flexDirection:'row', 
-              justifyContent:'space-between'
-              }}>
-            <Cardio width={80} height={80} style={{
-                padding:50,}}/>
-        
-            <Text style={{fontWeight:'bold', fontSize:18,color:"#fff"}}>Cardio</Text>
-        </TouchableOpacity>
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "#fff",
+        alignItems: "center", // set alignItems to center
+        padding: 20,
+      }}
+    >
+      <View>
+        <View>
+          <Text style={{ padding: 10, fontSize: 20 }}>By Type</Text>
         </View>
+        <ScrollView
+          horizontal={true}
+          style={{ flexDirection: "row", width: "100%", padding: 0 }}
+        >
+          <View style={{ padding: 10 }}>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("CardioScreen")}
+              style={{
+                backgroundColor: "#3EB489",
+                padding: 20,
+                width: 110,
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Cardio
+                width={30}
+                height={30}
+                style={{
+                  padding: 10,
+                }}
+              />
 
-        <View style={{padding:50}}>
-        <TouchableOpacity 
-          onPress={() => navigation.navigate('Main')}
-            style={{
-              backgroundColor:'#3EB489',
-              padding:20,
-              width:'70%',
-              flexDirection:'row', 
-              justifyContent:'space-between'
-              }}>
-           <Weights width={80} height={80} style={{
-          padding:50,}}/>
-             
-          <Text style={{fontWeight:'bold', fontSize:18,color:"#fff"}}>Weights</Text>
-        </TouchableOpacity>
-        </View>
+              <Text style={{ fontWeight: "bold", fontSize: 10, color: "#fff" }}>
+                Cardio
+              </Text>
+            </TouchableOpacity>
+          </View>
 
+          <View style={{ padding: 10 }}>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("WeightsScreen")}
+              style={{
+                backgroundColor: "#3EB489",
+                padding: 20,
+                width: 110,
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Weights
+                width={30}
+                height={30}
+                style={{
+                  padding: 10,
+                }}
+              />
+
+              <Text style={{ fontWeight: "bold", fontSize: 10, color: "#fff" }}>
+                Weights
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </View>
     </SafeAreaView>
-  )
-}
-const Main = ({navigation}) => {
+  );
+};
+const Main = ({ navigation }) => {
   return (
     // makes tags appear in right screen view
     <SafeAreaView
-    style={{
-      flex:1,
-      justifyContent:'center',
-      backgroundColor:'#fff',
-        alignItems: 'center', // set alignItems to center
-      padding:20
-    }}>
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "#fff",
+        alignItems: "center", // set alignItems to center
+        padding: 20,
+      }}
+    >
       <View>
-        <Text style={{
-          fontSize:30,
-          fontWeight:'bold',
-          color:'#3EB489',
-          padding: 10,
-        }}>
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: "bold",
+            color: "#3EB489",
+            padding: 10,
+          }}
+        >
           Gym Logger
         </Text>
-        </View>
+      </View>
 
-        <View>
-        <SambaShoe width={240} height={340} style={{
-          padding:50,}}/>
-        </View>
+      <View>
+        <SambaShoe
+          width={240}
+          height={340}
+          style={{
+            padding: 50,
+          }}
+        />
+      </View>
 
-
-        <TouchableOpacity 
-          onPress={() => navigation.navigate('Home')}
-            style={{
-              backgroundColor:'#3EB489',
-              padding:20,
-              width:'90%',
-              flexDirection:'row', 
-              justifyContent:'space-between'
-              }}>
-                
-          <Text style={{fontWeight:'bold', fontSize:18,color:"#fff"}}>Get Logging</Text>
-          <MaterialIcons name='arrow-forward-ios' size={22} color='#fff'/>
-        </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Home")}
+        style={{
+          backgroundColor: "#3EB489",
+          padding: 20,
+          width: "90%",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text style={{ fontWeight: "bold", fontSize: 18, color: "#fff" }}>
+          Get Logging
+        </Text>
+        <MaterialIcons name="arrow-forward-ios" size={22} color="#fff" />
+      </TouchableOpacity>
     </SafeAreaView>
   );
-}
+};
 
-const CardioScreen = ({navigation}) => {
-    return(
-      
-      <CardioLogger></CardioLogger>
-    )
-}
+const CardioScreen = ({ navigation }) => {
+  return <CardioLogger></CardioLogger>;
+};
 
-const WeightExercise = ({navigation}) => {
-    
-}
-
+const WeightsScreen = ({ navigation }) => {
+  <WeightliftingLogger></WeightliftingLogger>;
+};
 
 export default App;
-
---
+++ gymlogger/Screens/WeightsScreen.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
+import { Button, TextInput, IconButton } from "react-native-paper";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import moment from "moment";
+
+const WeightliftingLogger = () => {
+  const [exercise, setExercise] = useState("");
+  const [sets, setSets] = useState("");
+  const [reps, setReps] = useState("");
+  const [weight, setWeight] = useState("");
+
+  const handleSave = () => {
+    // Save the weightlifting exercise data to a database or file
+    console.log("Weightlifting exercise saved:", {
+      exercise,
+      sets,
+      reps,
+      weight,
+      date: moment().format("YYYY-MM-DD"),
+    });
+
+    // Clear the input fields
+    setExercise("");
+    setSets("");
+    setReps("");
+    setWeight("");
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          mode="outlined"
+          label="Exercise"
+          value={exercise}
+          onChangeText={(text) => setExercise(text)}
+        />
+        <TextInput
+          style={styles.input}
+          mode="outlined"
+          label="Sets"
+          value={sets}
+          onChangeText={(text) => setSets(text)}
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          mode="outlined"
+          label="Reps"
+          value={reps}
+          onChangeText={(text) => setReps(text)}
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          mode="outlined"
+          label="Weight (in lbs)"
+          value={weight}
+          onChangeText={(text) => setWeight(text)}
+          keyboardType="numeric"
+        />
+      </View>
+
+      <Button
+        style={styles.saveButton}
+        icon="content-save"
+        mode="contained"
+        onPress={handleSave}
+        disabled={!exercise || !sets || !reps || !weight}
+      >
+        Save
+      </Button>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    padding: 16,
+  },
+  inputContainer: {
+    marginBottom: 16,
+  },
+  input: {
+    marginBottom: 8,
+  },
+  saveButton: {
+    alignSelf: "center",
+    marginTop: 16,
+  },
+});
+
+export default WeightliftingLogger;
