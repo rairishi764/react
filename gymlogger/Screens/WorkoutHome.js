28b7a9c418bfb77ec48ae096a031c36edffd6f3d
sindabad764 <sindabad764@outlook.com>
2023-04-13 00:25:13 -0700

    WL : storing value of exercise type

--- gymlogger/Screens/WorkoutHome.js
+++ gymlogger/Screens/WorkoutHome.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   SafeAreaView,
   View,
@@ -10,20 +10,21 @@ import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CardioIcon from "../assets/cardio.svg";
-import WeightsIcon from "../assets/weights.svg";
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+
 
 const WorkoutHome = () => {
   const navigation = useNavigation();
 
+  const [selectedType, setSelectedType] = useState("");
+
   const styles = {
     container: {
       flex: 1,
-      justifyContent: "center",
       alignItems: "center",
       backgroundColor: "#fff",
       padding: 0,
-      width: '100%',
+      width: "100%",
     },
     header: {
       padding: 10,
@@ -34,7 +35,7 @@ const WorkoutHome = () => {
       padding: 10,
     },
     itemButton: {
-      backgroundColor: "#941796",
+      backgroundColor: "#6f3570",
       padding: 20,
       width: 150,
       flexDirection: "row",
@@ -51,16 +52,25 @@ const WorkoutHome = () => {
     },
   };
 
+  const handleTypeSelection = (type) => {
+    setSelectedType(type);
+    navigation.navigate("CardioScreen", { selectedType: type });
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <View>
+      <View style={{ height: 190 }}>
         <View>
           <Text style={styles.header}>By Type</Text>
         </View>
-        <ScrollView horizontal style={{ flexDirection: "row", padding: 0 }}>
+        <ScrollView
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          style={{ flexDirection: "row", padding: 0, flex: 1 }}
+        >
           <View style={styles.itemContainer}>
             <TouchableOpacity
-              onPress={() => navigation.navigate("CardioScreen")}
+              onPress={() => handleTypeSelection("Bodyweight")}
               style={styles.itemButton}
             >
               <View style={{ flexDirection: "column" }}>
@@ -72,7 +82,7 @@ const WorkoutHome = () => {
 
           <View style={styles.itemContainer}>
             <TouchableOpacity
-              onPress={() => navigation.navigate("CardioScreen")}
+              onPress={() => handleTypeSelection("Cardio")}
               style={styles.itemButton}
             >
               <View style={{ flexDirection: "column" }}>
@@ -81,58 +91,15 @@ const WorkoutHome = () => {
               </View>
             </TouchableOpacity>
           </View>
-
-
-          <View style={styles.itemContainer}>
-            <TouchableOpacity
-              onPress={() => navigation.navigate("CardioScreen")}
-              style={styles.itemButton}
-            >
-              <View style={{ flexDirection: "column" }}>
-                <CardioIcon width={50} height={50} style={styles.itemIcon} />
-                <Text style={styles.itemText}>Free Weights</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-
-          <View style={styles.itemContainer}>
-            <TouchableOpacity
-              onPress={() => navigation.navigate("CardioScreen")}
-              style={styles.itemButton}
-            >
-              <View style={{ flexDirection: "column" }}>
-                <CardioIcon width={50} height={50} style={styles.itemIcon} />
-                <Text style={styles.itemText}>Functional</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-
-          <View style={styles.itemContainer}>
-            <TouchableOpacity
-              onPress={() => navigation.navigate("CardioScreen")}
-              style={styles.itemButton}
-            >
-              <View style={{ flexDirection: "column" }}>
-                <CardioIcon width={50} height={50} style={styles.itemIcon} />
-                <Text style={styles.itemText}>Mobility</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-
-          <View style={styles.itemContainer}>
-            <TouchableOpacity
-              onPress={() => navigation.navigate("CardioScreen")}
-              style={styles.itemButton}
-            >
-              <View style={{ flexDirection: "column" }}>
-                <CardioIcon width={50} height={50} style={styles.itemIcon} />
-                <Text style={styles.itemText}>Resistance</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-
         </ScrollView>
       </View>
+
+      {/* Render the selected type */}
+      {selectedType !== "" && (
+        <View style={{ marginTop: 20 }}>
+          <Text>You selected: {selectedType}</Text>
+        </View>
+      )}
     </SafeAreaView>
   );
 };
