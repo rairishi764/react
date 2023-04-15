db67c4254f8644d2e913f0d000ff5a31c5970553
sindabad764 <sindabad764@outlook.com>
2023-04-14 20:33:51 -0700

    WL : cleaned up code for styles

--- gymlogger/Screens/WorkoutHome.js
+++ gymlogger/Screens/WorkoutHome.js
@@ -9,115 +9,64 @@ import {
   StyleSheet,
   Image,
 } from "react-native";
-import { Button, TextInput, IconButton } from "react-native-paper";
+import { TextInput } from "react-native-paper";
 import CardioIcon from "../assets/cardio.svg";
-import { useNavigation, useRoute } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 import BannerSlider from "../components/BannerSlider";
-import { windowWidth } from "../utils/Dimensions";
-import Carousel from "react-native-snap-carousel";
-import { sliderData } from "../components/data";
 
-const WorkoutHome = () => {
-  const styles = {
-    container: {
-      flex: 1,
-      alignItems: "center",
-      backgroundColor: "#fff",
-      padding: 0,
-      width: "100%",
-    },
-    header: {
-      padding: 10,
-      fontSize: 20,
-      fontWeight: "bold",
-    },
-    itemContainer: {
-      padding: 10,
-    },
-    itemButton: {
-      backgroundColor: "#6f3570",
-      padding: 20,
-      width: 150,
-      flexDirection: "row",
-      justifyContent: "space-between",
-    },
-    itemIcon: {
-      padding: 10,
-    },
-    itemText: {
-      fontWeight: "bold",
-      fontSize: 15,
-      color: "#fff",
-      padding: 5,
-    },
-  };
+export default function WorkoutHome () {
+  const [selectedType, setSelectedType] = useState("");
 
   const navigation = useNavigation();
 
-  const [selectedType, setSelectedType] = useState("");
-  const renderBanner = (item, index) => {
-    return <BannerSlider data={item} />;
-  };
   const handleTypeSelection = (type, screen) => {
     setSelectedType(type);
     navigation.navigate(screen, { selectedType: type });
   };
 
+  const renderBanner = (item, index) => {
+    return <BannerSlider data={item} />;
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          marginBottom: 20,
-          borderRadius: 8,
-          paddingHorizontal: 10,
-          paddingVertical: 8,
-          width: "100%",
-        }}
-      >
+      <View style={styles.greeting}>
         <Text>Hello John Doe</Text>
         <ImageBackground
           source={require("../assets/user.jpg")}
-          style={{ width: 35, height: 35 }}
-          imageStyle={{ borderRadius: 25 }}
+          style={styles.userImage}
+          imageStyle={styles.userImageBorder}
         />
       </View>
-      <View style={{ flexDirection: "row" }}>
-        <TextInput placeholder="Search" style={{ width: "95%" }} />
+
+      <View style={styles.searchContainer}>
+        <TextInput placeholder="Search" style={styles.searchInput} />
       </View>
 
-      <View
-        style={{
-          marginVertical: 15,
-          flexDirection: "row",
-          justifyContent: "space-between",
-          width: "95%",
-        }}
-      >
-        <Text style={{ fontSize: 18 }}>Top Workouts</Text>
+      <View style={styles.topWorkoutsContainer}>
+        <Text style={styles.topWorkoutsText}>Top Workouts</Text>
         <TouchableOpacity onPress={() => {}}>
-          <Text style={{ color: "#941796" }}>See all</Text>
+          <Text style={styles.seeAllText}>See all</Text>
         </TouchableOpacity>
       </View>
 
-        <BannerSlider></BannerSlider>
+      <BannerSlider />
 
-      <View style={{ height: 190 }}>
-        <View>
-          <Text style={styles.header}>By Type</Text>
-        </View>
+      <View style={styles.byTypeContainer}>
+        <Text style={styles.byTypeHeader}>By Type</Text>
         <ScrollView
           showsHorizontalScrollIndicator={false}
           horizontal
-          style={{ flexDirection: "row", padding: 0, flex: 1 }}
+          style={styles.byTypeScrollView}
         >
           <View style={styles.itemContainer}>
             <TouchableOpacity
-              onPress={() => handleTypeSelection("Bodyweight", "WeightsScreen")}
+              onPress={() =>
+                handleTypeSelection("Bodyweight", "WeightsScreen")
+              }
               style={styles.itemButton}
             >
-              <View style={{ flexDirection: "column" }}>
+              <View style={styles.itemButtonContent}>
                 <CardioIcon width={50} height={50} style={styles.itemIcon} />
                 <Text style={styles.itemText}>Bodyweight</Text>
               </View>
@@ -129,7 +78,7 @@ const WorkoutHome = () => {
               onPress={() => handleTypeSelection("Cardio", "CardioScreen")}
               style={styles.itemButton}
             >
-              <View style={{ flexDirection: "column" }}>
+              <View style={styles.itemButtonContent}>
                 <CardioIcon width={50} height={50} style={styles.itemIcon} />
                 <Text style={styles.itemText}>Cardio</Text>
               </View>
@@ -138,9 +87,8 @@ const WorkoutHome = () => {
         </ScrollView>
       </View>
 
-      {/* Render the selected type */}
       {selectedType !== "" && (
-        <View style={{ marginTop: 20 }}>
+        <View style={styles.selectedTypeContainer}>
           <Text>You selected: {selectedType}</Text>
         </View>
       )}
@@ -148,21 +96,90 @@ const WorkoutHome = () => {
   );
 };
 
-export default WorkoutHome;
-
-const styles1 = StyleSheet.create({
-  slide: {
-    backgroundColor: "white",
-    borderRadius: 5,
-    padding: 20,
-    marginHorizontal: 10,
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 0,
+    width: "100%",
   },
-  title: {
+  header: {
+    padding: 10,
     fontSize: 20,
     fontWeight: "bold",
-    marginBottom: 10,
   },
-  text: {
-    fontSize: 16,
+  itemContainer: {
+    padding: 10,
   },
+  itemButton: {
+    backgroundColor: "#6f3570",
+    padding: 20,
+    width: 150,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  itemIcon: {
+    padding: 10,
+  },
+  itemText: {
+    fontWeight: "bold",
+    fontSize: 15,
+    color: "#fff",
+    padding: 5,
+  },
+  greeting: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    width: "100%",
+  },
+  userImage: {
+    width: 35,
+    height: 35,
+    borderRadius: 25,
+  },
+  searchContainer: {
+    flexDirection: "row",
+  },
+  searchInput: {
+    width: "95%",
+  },
+  topWorkoutsContainer: {
+    marginVertical: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "95%",
+  },
+  topWorkoutsTitle: {
+    fontSize: 18,
+  },
+  seeAll: {
+    color: "#941796",
+  },
+  byTypeContainer: {
+    height: 190,
+  },
+  byTypeHeader: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  byTypeScrollView: {
+    flexDirection: "row",
+    padding: 0,
+    flex: 1,
+  },
+  selectedTypeContainer: {
+    marginTop: 20,
+  },
+  selectedTypeText: {
+    fontSize: 18,
+  },
+  topWorkoutsText:{
+    fontWeight:"bold",
+    fontSize: 20,
+  }
 });
