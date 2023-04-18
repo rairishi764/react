3bcd2d56ffbfcee68ad1011a07d9f66895a97945
sindabad764 <sindabad764@outlook.com>
2023-04-17 18:57:27 -0700

    WL : changed wghtscrn weight select to searchable

--- gymlogger/Screens/WeightsScreen.js
+++ gymlogger/Screens/WeightsScreen.js
@@ -1,29 +1,69 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
   SafeAreaView,
-  ScrollView
+  ScrollView,
+  Image,
+  FlatList,
 } from "react-native";
 import { Button, TextInput, IconButton } from "react-native-paper";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import moment from "moment";
-import { useNavigation } from '@react-navigation/native';
-import { Slider } from 'react-native-elements';
+import { useNavigation } from "@react-navigation/native";
+import { Slider } from "react-native-elements";
 
 const WeightliftingLogger = () => {
-  const navigation = useNavigation();  
-  const [exercise, setExercise] = useState("");
+  const navigation = useNavigation();
+
   const [sets, setSets] = useState(1);
   const [reps, setReps] = useState([0]);
   const [weight, setWeight] = useState([0]);
+  const weightWorkoutType = [
+    { workout: "Bench Press" },
+    { workout: "Squats" },
+    { workout: "Deadlifts" },
+    { workout: "Shoulder Press" },
+    { workout: "Barbell Rows" },
+    { workout: "Bicep Curls" },
+    { workout: "Tricep Extensions" },
+    { workout: "Lunges" },
+    { workout: "Leg Press" },
+    { workout: "Pull-Ups" },
+    { workout: "Dips" },
+    { workout: "Leg Curls" },
+    { workout: "Leg Extensions" },
+    { workout: "Calf Raises" },
+    { workout: "Abdominal Crunches" },
+    { workout: "Planks" },
+    { workout: "Russian Twists" },
+    { workout: "Cable Rows" },
+    { workout: "Chest Flys" },
+    { workout: "Lat Pulldowns" },
+  ];
+
+  const [search, setSearch] = useState("");
+  const [clicked, setClicked] = useState(false);
+  const [data, setData] = useState(weightWorkoutType);
+  const [selectedWeightWorkout, setSelectedWeightWorkout] = useState("");
+  const searchRef = useRef();
+  const onSearch = (search) => {
+    if (search !== "") {
+      let tempData = data.filter((item) => {
+        return item.workout.toLowerCase().indexOf(search.toLowerCase()) > -1;
+      });
+      setData(tempData);
+    } else {
+      setData(weightWorkoutType);
+    }
+  };
 
   const handleSave = () => {
     // Save the weightlifting exercise data to a database or file
     console.log("Weightlifting exercise saved:", {
-      exercise,
+      selectedWeightWorkout,
       sets,
       reps,
       weight,
@@ -31,7 +71,6 @@ const WeightliftingLogger = () => {
     });
 
     // Clear the input fields
-    setExercise("");
     setSets(1);
     setReps([0]);
     setWeight([0]);
@@ -45,30 +84,32 @@ const WeightliftingLogger = () => {
           <Text style={styles.label}>{`Set ${i}`}</Text>
           <View style={styles.row}>
             <View style={styles.sliderContainer}>
-              <Text style={styles.label}>{`Reps: ${reps[i-1]}`}</Text>
+              <Text style={styles.label}>{`Reps: ${reps[i - 1]}`}</Text>
               <Slider
                 style={styles.slider}
                 minimumValue={0}
                 maximumValue={20}
                 step={1}
-                value={reps[i-1]}
+                value={reps[i - 1]}
                 onValueChange={(value) => handleRepsChange(i, value)}
-                minimumTrackTintColor='#9971b0'
-                thumbTintColor='#9971b0'
+                minimumTrackTintColor="#9971b0"
+                thumbTintColor="#9971b0"
                 thumbStyle={{ width: 10, height: 25 }}
               />
             </View>
             <View style={styles.sliderContainer}>
-              <Text style={styles.label}>{`Weight (in lbs): ${weight[i-1]}`}</Text>
+              <Text style={styles.label}>{`Weight (in lbs): ${
+                weight[i - 1]
+              }`}</Text>
               <Slider
                 style={styles.slider}
                 minimumValue={0}
                 maximumValue={200}
                 step={5}
-                value={weight[i-1]}
+                value={weight[i - 1]}
                 onValueChange={(value) => handleWeightChange(i, value)}
-                minimumTrackTintColor='#6b4c7d'
-                thumbTintColor='#6b4c7d'
+                minimumTrackTintColor="#6b4c7d"
+                thumbTintColor="#6b4c7d"
                 thumbStyle={{ width: 10, height: 25 }}
               />
             </View>
@@ -86,7 +127,7 @@ const WeightliftingLogger = () => {
       return newReps;
     });
   };
-  
+
   const handleWeightChange = (index, value) => {
     setWeight((prevWeight) => {
       const newWeight = [...prevWeight];
@@ -94,7 +135,6 @@ const WeightliftingLogger = () => {
       return newWeight;
     });
   };
-  
 
   return (
     <SafeAreaView style={styles.container}>
@@ -104,30 +144,120 @@ const WeightliftingLogger = () => {
         style={{ flexDirection: "column", padding: 0, flex: 1 }}
       >
         <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            mode="outlined"
-            label="Exercise"
-            value={exercise}
-            onChangeText={(text) => setExercise(text)}
-          />
+          <View>
+            <TouchableOpacity
+              style={{
+                width: "90%",
+                height: 50,
+                borderRadius: 10,
+                borderWidth: 0.5,
+                alignSelf: "center",
+                marginTop: 10,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                paddingLeft: 15,
+                paddingRight: 15,
+              }}
+              onPress={() => {
+                setClicked(!clicked);
+              }}
+            >
+              <Text style={{ fontWeight: "600" }}>
+                {selectedWeightWorkout == ""
+                  ? "Select Workout"
+                  : selectedWeightWorkout}
+              </Text>
+              {clicked ? (
+                <Image
+                  source={require("../assets/upload.png")}
+                  style={{ width: 20, height: 20 }}
+                />
+              ) : (
+                <Image
+                  source={require("../assets/dropdown.png")}
+                  style={{ width: 20, height: 20 }}
+                />
+              )}
+            </TouchableOpacity>
+            {clicked ? (
+              <View
+                style={{
+                  elevation: 5,
+                  marginTop: 20,
+                  height: 300,
+                  alignSelf: "center",
+                  width: "90%",
+                  backgroundColor: "#fff",
+                  borderRadius: 10,
+                }}
+              >
+                <TextInput
+                  placeholder="Search.."
+                  value={search}
+                  ref={searchRef}
+                  onChangeText={(txt) => {
+                    onSearch(txt);
+                    setSearch(txt);
+                  }}
+                  style={{
+                    width: "100%",
+                    height: 50,
+                    alignSelf: "center",
+                    borderWidth: 0.2,
+                    borderColor: "#8e8e8e",
+                    borderRadius: 7,
+                    marginTop: 20,
+                    paddingLeft: 20,
+                  }}
+                />
 
-<View>
-  <Text style={styles.label}>Sets: {sets}</Text>
-  <Slider
-    style={{ marginTop: 10 }}
-    minimumValue={1}
-    maximumValue={10}
-    step={1}
-    value={sets}
-    onValueChange={(value) => setSets(value)}
-    minimumTrackTintColor="#6a2194"
-    maximumTrackTintColor="#f3e8fa"
-    thumbTintColor="#5b1c80"
-    thumbStyle={{ width: 10, height: 25 }}
-  />
-</View>
+                <FlatList
+                  data={data}
+                  renderItem={({ item, index }) => {
+                    return (
+                      <TouchableOpacity
+                        style={{
+                          width: "100%",
+                          alignSelf: "center",
+                          height: 50,
+                          justifyContent: "center",
+                          borderBottomWidth: 0.5,
+                          borderColor: "#8e8e8e",
+                        }}
+                        onPress={() => {
+                          setSelectedWeightWorkout(item.workout);
+                          setClicked(!clicked);
+                          onSearch("");
+                          setSearch("");
+                        }}
+                      >
+                        <Text style={{ fontWeight: "600" }}>
+                          {item.workout}
+                        </Text>
+                      </TouchableOpacity>
+                    );
+                  }}
+                />
+              </View>
+            ) : null}
+          </View>
 
+          <View>
+            <Text style={styles.label}>Sets: {sets}</Text>
+            <Slider
+              style={{ marginTop: 10 }}
+              minimumValue={1}
+              maximumValue={10}
+              step={1}
+              value={sets}
+              onValueChange={(value) => setSets(value)}
+              minimumTrackTintColor="#6a2194"
+              maximumTrackTintColor="#f3e8fa"
+              thumbTintColor="#5b1c80"
+              thumbStyle={{ width: 10, height: 25 }}
+            />
+          </View>
 
           {renderSetInputs()}
         </View>
@@ -137,62 +267,65 @@ const WeightliftingLogger = () => {
           mode="contained"
           onPress={handleSave}
           disabled={
-            !exercise || !sets || reps.length < sets || weight.length < sets
+            !selectedWeightWorkout ||
+            !sets ||
+            reps.length < sets ||
+            weight.length < sets
           }
         >
           Save
         </Button>
       </ScrollView>
     </SafeAreaView>
-  )};
+  );
+};
 
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      backgroundColor: "#fff",
-      padding: 16,
-    },
-    inputContainer: {
-      marginBottom: 16,
-      padding: 35,
-    },
-    input: {
-      marginBottom: 8,
-    },
-    saveButton: {
-      alignSelf: "center",
-      marginTop: 16,
-    },
-    setRowContainer: {
-      flexDirection: "row",
-      alignItems: "center",
-      marginBottom: 16,
-    },
-    setNumber: {
-      fontWeight: "bold",
-      marginRight: 8,
-      fontSize: 16,
-    },
-    sliderContainer: {
-      flexDirection: "row",
-      alignItems: "center",
-      marginBottom: 16,
-    },
-    slider: {
-      flex: 1,
-      marginLeft: 16,
-      marginRight: 8
-    },
-    sliderValue: {
-      minWidth: 50,
-      textAlign: "center",
-    },
-    label: {
-      fontSize: 12,
-      marginTop: 5,
-    }    
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    padding: 16,
+  },
+  inputContainer: {
+    marginBottom: 16,
+    padding: 35,
+  },
+  input: {
+    marginBottom: 8,
+  },
+  saveButton: {
+    alignSelf: "center",
+    marginTop: 16,
+  },
+  setRowContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 16,
+  },
+  setNumber: {
+    fontWeight: "bold",
+    marginRight: 8,
+    fontSize: 16,
+  },
+  sliderContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 16,
+  },
+  slider: {
+    flex: 1,
+    marginLeft: 16,
+    marginRight: 8,
+  },
+  sliderValue: {
+    minWidth: 50,
+    textAlign: "center",
+  },
+  label: {
+    fontSize: 12,
+    marginTop: 5,
+  },
+});
 
 export default WeightliftingLogger;
