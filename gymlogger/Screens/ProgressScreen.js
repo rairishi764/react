133b1a44c34d41f75f1f3a399b0bb1ea4f20c4fd
sindabad764 <sindabad764@outlook.com>
2023-04-18 15:44:08 -0700

    WL : sample video play screen

--- gymlogger/Screens/ProgressScreen.js
+++ gymlogger/Screens/ProgressScreen.js
@@ -1,104 +1,42 @@
 import React, { useState } from 'react';
-import { View, TouchableOpacity, Text, Modal, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Video } from 'expo-av';
 
-const MyComponent = () => {
-  const [modalVisible, setModalVisible] = useState(false);
-  const [selectedItem, setSelectedItem] = useState(null);
-  const [searchQuery, setSearchQuery] = useState('');
+export default function App() {
+  const [videoURI, setVideoURI] = useState('http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4');
+  const [videoStatus, setVideoStatus] = useState({});
 
-  const data = [
-    { id: 1, name: 'Item 1', description: 'This is the description for item 1' },
-    { id: 2, name: 'Item 2', description: 'This is the description for item 2' },
-    { id: 3, name: 'Item 3', description: 'This is the description for item 3' },
-  ];
-
-  const handlePress = (item) => {
-    setSelectedItem(item);
-    setModalVisible(true);
-  };
-
-  const handleSearch = (query) => {
-    setSearchQuery(query);
-  };
-
-  const renderItem = (item) => {
-    if (searchQuery && !item.name.toLowerCase().includes(searchQuery.toLowerCase())) {
-      return null; // skip rendering if item doesn't match search query
+  const handlePlayPause = async () => {
+    if (videoStatus.isPlaying) {
+      await videoStatus.pauseAsync();
+    } else {
+      await videoStatus.playAsync();
     }
-
-    return (
-      <TouchableOpacity key={item.id} onPress={() => handlePress(item)}>
-        <View style={styles.item}>
-          <Text style={styles.title}>{item.name}</Text>
-          <Text style={styles.description}>{item.description}</Text>
-        </View>
-      </TouchableOpacity>
-    );
   };
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.searchInput}
-        placeholder="Search"
-        value={searchQuery}
-        onChangeText={handleSearch}
+      <Video
+        source={{ uri: videoURI }}
+        ref={ref => setVideoStatus(ref)}
+        style={styles.video}
+        useNativeControls
+        resizeMode="contain"
       />
-      {data.map(renderItem)}
-      {selectedItem && (
-        <Modal visible={modalVisible} animationType="slide">
-          <View style={styles.modalContainer}>
-            <Text style={styles.title}>{selectedItem.name}</Text>
-            <Text style={styles.description}>{selectedItem.description}</Text>
-            <TouchableOpacity onPress={() => setModalVisible(false)}>
-              <Text style={styles.closeButton}>Close</Text>
-            </TouchableOpacity>
-          </View>
-        </Modal>
-      )}
+      <Button title={videoStatus.isPlaying ? "Pause" : "Play"} onPress={handlePlayPause} />
     </View>
   );
-};
+}
 
-const styles = {
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
-  },
-  item: {
-    padding: 16,
-    marginBottom: 16,
-    backgroundColor: '#f2f2f2',
-    borderRadius: 8,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 8,
-  },
-  description: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    padding: 16,
+    justifyContent: 'center',
   },
-  closeButton: {
-    fontSize: 18,
-    color: 'blue',
-    marginTop: 16,
+  video: {
+    width: '100%',
+    height: 300,
   },
-  searchInput: {
-    height: 40,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 8,
-    padding: 8,
-    marginBottom: 16,
-  },
-};
-
-export default MyComponent;
+});
