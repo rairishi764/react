2656cadda8803c661ba316bc4fb63ebdd9d8f317
sindabad764 <sindabad764@outlook.com>
2023-04-18 14:50:15 -0700

    WL : set model on exercisedetail search screen

--- gymlogger/Screens/ExerciseDetailScreen.js
+++ gymlogger/Screens/ExerciseDetailScreen.js
@@ -1,244 +1,170 @@
 import React, { useState } from "react";
 import {
-  SafeAreaView,
   View,
+  TouchableOpacity,
+  Text,
+  Modal,
   TextInput,
   FlatList,
-  TouchableOpacity,
-  StyleSheet,
-  Text,
-  Modal
 } from "react-native";
-import Video from 'react-native-video';
+import Video from "react-native-video";
 
-
-const DATA = [  {    id: "1",    name: "Bench Press",    muscles: ["Chest", "Triceps"],
+const DATA = [
+  {
+    id: "1",
+    name: "Bench Press",
+    muscles: ["Chest", "Triceps"],
     videoUrl: "https://www.youtube.com/watch?v=gRVjAtPip0Y",
-    description: "The bench press is a chest exercise that targets the pectoral muscles. Begin by lying on the bench with your feet on the ground and your back flat. Hold the bar with an overhand grip and lower it to your chest. Pause for a moment, and then push the bar back up to the starting position.",
+    description:
+      "The bench press is a chest exercise that targets the pectoral muscles. Begin by lying on the bench with your feet on the ground and your back flat. Hold the bar with an overhand grip and lower it to your chest. Pause for a moment, and then push the bar back up to the starting position.",
   },
   {
     id: "2",
     name: "Squat",
     muscles: ["Quadriceps", "Glutes", "Hamstrings"],
     videoUrl: "https://www.youtube.com/watch?v=ULT0jl6lwrI",
-    description: "The squat is a compound exercise that targets the muscles of the lower body. Begin by standing with your feet shoulder-width apart and your toes pointed slightly outward. Hold a barbell on your upper back and lower your body until your thighs are parallel to the ground. Pause for a moment, and then push yourself back up to the starting position.",
+    description:
+      "The squat is a compound exercise that targets the muscles of the lower body. Begin by standing with your feet shoulder-width apart and your toes pointed slightly outward. Hold a barbell on your upper back and lower your body until your thighs are parallel to the ground. Pause for a moment, and then push yourself back up to the starting position.",
   },
   {
     id: "3",
     name: "Deadlift",
     muscles: ["Lower Back", "Glutes", "Hamstrings"],
     videoUrl: "https://www.youtube.com/watch?v=1ZXobuFvFJ8",
-    description: "The deadlift is a compound exercise that targets the muscles of the lower back, glutes, and hamstrings. Begin by standing with your feet shoulder-width apart and your toes pointed slightly outward. Bend down and grab the bar with an overhand grip. Lift the bar by straightening your legs and pulling your hips forward. Pause for a moment, and then lower the bar back down to the ground.",
+    description:
+      "The deadlift is a compound exercise that targets the muscles of the lower back, glutes, and hamstrings. Begin by standing with your feet shoulder-width apart and your toes pointed slightly outward. Bend down and grab the bar with an overhand grip. Lift the bar by straightening your legs and pulling your hips forward. Pause for a moment, and then lower the bar back down to the ground.",
+  },
+  {
+    id: "4",
+    name: "Pull-up",
+    muscles: ["Back", "Biceps"],
+    videoUrl: "https://www.youtube.com/watch?v=eGo4IYlbE5g",
+    description:
+      "The pull-up is an upper body exercise that targets the back and biceps. Begin by hanging from a bar with your palms facing away from your body. Pull your body up until your chin is above the bar. Pause for a moment, and then lower yourself back down to the starting position.",
+  },
+  {
+    id: "5",
+    name: "Push-up",
+    muscles: ["Chest", "Shoulders", "Triceps"],
+    videoUrl: "https://www.youtube.com/watch?v=IODxDxX7oi4",
+    description:
+      "The push-up is a classic exercise that targets the chest, shoulders, and triceps. Begin by getting into a plank position with your hands slightly wider than shoulder-width apart. Lower your body until your chest nearly touches the ground. Push yourself back up to the starting position.",
+  },
+  {
+    id: "6",
+    name: "Plank",
+    muscles: ["Core"],
+    videoUrl: "https://www.youtube.com/watch?v=pSHjTRCQxIw",
+    description:
+      "The plank is a core exercise that strengthens the muscles of your abs, back, and hips. Begin by getting into a push-up position, but instead of lowering yourself down, hold your body in a straight line from your head to your heels. Keep your abs and glutes tight, and hold for as long as you can.",
   },
 ];
 
+const MyComponent = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
 
-const ExerciseDetailScreen = () => {
-  const [query, setQuery] = useState("");
-  const [workouts, setWorkouts] = useState(
-    DATA.map((item) => ({
-      ...item,
-      showModal: false,
-    }))
-  );
-  const [selectedWorkout, setSelectedWorkout] = useState(null);
-
-  const toggleModal = (itemId) => {
-    setWorkouts((prevWorkouts) =>
-      prevWorkouts.map((item) =>
-        item.id === itemId ? { ...item, showModal: !item.showModal } : item
-      )
-    );
-    setSelectedWorkout(workouts.find((item) => item.id === itemId));
+  const handlePress = (item) => {
+    setSelectedItem(item);
+    setModalVisible(true);
   };
 
+  const renderItem = ({ item }) => (
+    <TouchableOpacity key={item.id} onPress={() => handlePress(item)}>
+      <View style={styles.item}>
+        <Text style={styles.title}>{item.name}</Text>
+        <Text style={styles.description}>
+          {item.description.slice(0, 100)}...
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
 
   const handleSearch = (text) => {
-    setQuery(text);
+    setSearchQuery(text);
   };
 
-  const renderWorkout = ({ item }) => (
-    <TouchableOpacity style={styles.item} onPress={() => toggleModal(item.id)}>
-      <Text style={styles.name}>{item.name}</Text>
-      <Text style={styles.muscles}>{item.muscles.join(", ")}</Text>
-      <Text style={styles.videoUrl}>{item.videoUrl}</Text>
-      <Text style={styles.description} numberOfLines={1}>
-        {item.description}
-      </Text>
-      {item.showModal && (
-        <View style={styles.modalContainer}>
-          <View style={styles.modal}>
-            <Text style={styles.modalTitle}>{item.name}</Text>
-            <Text style={styles.modalDescription}>{item.description}</Text>
-            <TouchableOpacity
-              style={styles.closeButton}
-              onPress={() => toggleModal(item.id)}
-            >
-              <Text style={styles.closeButtonText}>Close</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      )}
-    </TouchableOpacity>
-  ); 
-
-  const filteredData = DATA.filter(
-    (item) =>
-      item.name.toLowerCase().includes(query.toLowerCase()) ||
-      item.muscles.some((muscle) => muscle.toLowerCase().includes(query.toLowerCase())) ||
-      item.videoUrl.toLowerCase().includes(query.toLowerCase())
-  );
+  const filteredData = DATA.filter((item) => {
+    const muscleMatch = item.muscles.find((muscle) =>
+      muscle.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    return (
+      item.name.toLowerCase().includes(searchQuery.toLowerCase()) || muscleMatch
+    );
+  });
 
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <TextInput
-        style={styles.searchBar}
-        placeholder="Search for a workout"
+        style={styles.searchInput}
+        placeholder="Search"
         onChangeText={handleSearch}
-        value={query}
+        value={searchQuery}
       />
       <FlatList
         data={filteredData}
-        renderItem={renderWorkout}
+        renderItem={renderItem}
         keyExtractor={(item) => item.id}
+        contentContainerStyle={styles.listContainer}
       />
-    </SafeAreaView>
+      {selectedItem && (
+        <Modal visible={modalVisible} animationType="slide">
+          <View style={styles.modalContainer}>
+            <Text style={styles.title}>{selectedItem.name}</Text>
+            <Text style={styles.description}>{selectedItem.description}</Text>
+
+            <TouchableOpacity onPress={() => setModalVisible(false)}>
+              <Text style={styles.closeButton}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      )}
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
+    padding: 16,
   },
-  searchBar: {
-    backgroundColor: "#F2F2F2",
-    padding: 10,
-    margin: 10,
-    borderRadius: 10,
+  listContainer: {
+    paddingBottom: 80,
   },
   item: {
-    backgroundColor: "#FFFFFF",
-    padding: 10,
-    marginVertical: 2,
-    marginHorizontal: 16,
+    padding: 16,
+    marginBottom: 16,
+    backgroundColor: "#f2f2f2",
+    borderRadius: 8,
   },
-  name: {
-    fontSize: 16,
+  title: {
+    fontSize: 18,
     fontWeight: "bold",
+    marginBottom: 8,
   },
-  muscles: {
-    fontSize: 14,
+  description: {
+    fontSize: 16,
+    marginBottom: 8,
   },
-  videoUrl: {
-    fontSize: 12,
-    color: "#666666",
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
   },
-    container: {
-      flex: 1,
-    },
-    searchBar: {
-      backgroundColor: "#F2F2F2",
-      padding: 10,
-      margin: 10,
-      borderRadius: 10,
-    },
-    item: {
-      backgroundColor: "#FFFFFF",
-      padding: 10,
-      marginVertical: 2,
-      marginHorizontal: 16,
-    },
-    name: {
-      fontSize: 16,
-      fontWeight: "bold",
-    },
-    muscles: {
-      fontSize: 14,
-    },
-    videoUrl: {
-      fontSize: 12,
-      color: "#666666",
-    },
-    description: {
-      fontSize: 14,
-      color: "#666666",
-    },
-    modalContainer: {
-      position: "absolute",
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      backgroundColor: "rgba(0, 0, 0, 0.5)",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    modal: {
-      backgroundColor: "#FFFFFF",
-      padding: 20,
-      borderRadius: 10,
-      alignItems: "center",
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 4,
-      elevation: 5,
-    },
-    modalTitle: {
-      fontSize: 18,
-      fontWeight: "bold",
-      marginBottom: 10,
-    },
-    modalDescription: {
-      fontSize: 14,
-      marginBottom: 10,
-    },
-    closeButton: {
-      backgroundColor: "#FF0000",
-      padding: 10,
-      borderRadius: 5,
-    },
-    closeButtonText: {
-      color: "#FFFFFF",
-      fontSize: 16,
-      fontWeight: "bold",
-    },
-    modalContainer: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "rgba(0,0,0,0.5)",
-      },
-      modal: {
-        backgroundColor: "#FFFFFF",
-        borderRadius: 10,
-        padding: 20,
-        width: "80%",
-      },
-      modalTitle: {
-        fontSize: 20,
-        fontWeight: "bold",
-        marginBottom: 10,
-      },
-      modalDescription: {
-        fontSize: 16,
-        marginBottom: 10,
-      },
-      closeButton: {
-        backgroundColor: "#F44336",
-        borderRadius: 5,
-        padding: 10,
-        alignSelf: "flex-end",
-      },
-      closeButtonText: {
-        color: "#FFFFFF",
-        fontSize: 16,
-        fontWeight: "bold",
-      },
-    
-});
+  closeButton: {
+    fontSize: 18,
+    color: "blue",
+    marginTop: 16,
+  },
+  searchInput: {
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    marginBottom: 16,
+  },
+};
 
-export default ExerciseDetailScreen;
+export default MyComponent;
--- gymlogger/Screens/ProgressScreen.js
+++ gymlogger/Screens/ProgressScreen.js
@@ -1,63 +1,104 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Modal, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, Text, Modal, TextInput } from 'react-native';
 
-const ProgressScreen = () => {
+const MyComponent = () => {
   const [modalVisible, setModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const data = [
+    { id: 1, name: 'Item 1', description: 'This is the description for item 1' },
+    { id: 2, name: 'Item 2', description: 'This is the description for item 2' },
+    { id: 3, name: 'Item 3', description: 'This is the description for item 3' },
+  ];
+
+  const handlePress = (item) => {
+    setSelectedItem(item);
+    setModalVisible(true);
+  };
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
+  const renderItem = (item) => {
+    if (searchQuery && !item.name.toLowerCase().includes(searchQuery.toLowerCase())) {
+      return null; // skip rendering if item doesn't match search query
+    }
+
+    return (
+      <TouchableOpacity key={item.id} onPress={() => handlePress(item)}>
+        <View style={styles.item}>
+          <Text style={styles.title}>{item.name}</Text>
+          <Text style={styles.description}>{item.description}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.button}>
-        <Text style={styles.buttonText}>Open Modal</Text>
-      </TouchableOpacity>
-
-      <Modal
-        animationType="slide"
-        visible={modalVisible}
-        onRequestClose={() => setModalVisible(false)}
-      >
-        <View style={styles.modalContainer}>
-          <Text style={styles.modalText}>This is a modal!</Text>
-          <TouchableOpacity onPress={() => setModalVisible(false)} style={styles.closeButton}>
-            <Text style={styles.buttonText}>Close Modal</Text>
-          </TouchableOpacity>
-        </View>
-      </Modal>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Search"
+        value={searchQuery}
+        onChangeText={handleSearch}
+      />
+      {data.map(renderItem)}
+      {selectedItem && (
+        <Modal visible={modalVisible} animationType="slide">
+          <View style={styles.modalContainer}>
+            <Text style={styles.title}>{selectedItem.name}</Text>
+            <Text style={styles.description}>{selectedItem.description}</Text>
+            <TouchableOpacity onPress={() => setModalVisible(false)}>
+              <Text style={styles.closeButton}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      )}
     </View>
   );
 };
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    padding: 16,
   },
-  button: {
-    backgroundColor: '#4CAF50',
-    padding: 10,
-    borderRadius: 5,
+  item: {
+    padding: 16,
+    marginBottom: 16,
+    backgroundColor: '#f2f2f2',
+    borderRadius: 8,
   },
-  buttonText: {
-    color: '#fff',
+  title: {
     fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 8,
   },
   modalContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  modalText: {
-    fontSize: 24,
-    marginBottom: 20,
+    padding: 16,
   },
   closeButton: {
-    backgroundColor: '#FF5722',
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 20,
+    fontSize: 18,
+    color: 'blue',
+    marginTop: 16,
   },
-});
+  searchInput: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    padding: 8,
+    marginBottom: 16,
+  },
+};
 
-export default ProgressScreen;
+export default MyComponent;
