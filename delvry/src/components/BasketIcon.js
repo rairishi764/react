93a8b3529659ab94ee034b6cf02101e896f7cdad
sindabad764 <sindabad764@outlook.com>
2023-09-07 17:22:55 -0700

    WorkoutLog: Created base app with nativewinds

--- delvry/src/components/BasketIcon.js
+++ delvry/src/components/BasketIcon.js
@@ -8,6 +8,8 @@ const BasketIcon = () => {
     const items = useSelector(selectBasketItems)
     const navigation = useNavigation()
     const basketTotal = useSelector(selectBasketTotal)
+
+    if (items.length === 0) return null
   return (
     <View className='absolute bottom-10 w-full z-50'>
         <TouchableOpacity onPress={()=>navigation.navigate("Basket")} 
--- delvry/src/screens/BasketScreen.js
+++ delvry/src/screens/BasketScreen.js
@@ -1,13 +1,64 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Image, ScrollView } from 'react-native'
+import React, {useEffect, useState} from 'react'
 import { useNavigation } from '@react-navigation/native'
+import { selectRestaurant } from '../../features/restaurantSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectBasketItems } from '../../features/basketSlice'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { XCircleIcon } from 'react-native-heroicons/outline'
 
 const BasketScreen = () => {
-    const navigation = useNavigation();
+    const navigation = useNavigation()
+    const restaurant = useSelector(selectRestaurant);
+    const items = useSelector(selectBasketItems)
+    const [groupedItemsInBasket, setGroupedItemsInBasket] = useState([])
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+      const groupItems = items.reduce((results, item)=>{
+        (results[items.id] = results[items.id] || []).push(item)
+        return results
+      },{})
+      setGroupedItemsInBasket(groupItems)
+    },[items])
+    console.log(groupedItemsInBasket)
   return (
-    <View>
-      <Text>BasketScreen</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-1 bg-gray-100">
+        <View className="p-5 border-b border-[#00CCBB] bg-white shadow-xs">
+          <View>
+            <Text className="text-lg font-bold text-center">Basket</Text>
+            <Text className="text-center text-gray-400">
+              {restaurant.title}
+              </Text>
+          </View>
+
+          <TouchableOpacity
+            onPress={navigation.goBack}
+            className="rounded-full bg-gray-100 absolute top-3 right-5">
+              <XCircleIcon color="#00CCBB" height={50} width={50}/>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View>
+        <Image source = {{
+          uri:"https://links.papareact.com/wru",
+          className:"h-7 w-7 bg-gray-300 p-4 rounded-full"
+        }}/>
+        <Text className="flex-1">Deliver in 50-70 min</Text>
+        <TouchableOpacity>
+          <Text className="text-[#00CCBB]">Change</Text>
+        </TouchableOpacity>
+      </View>
+      <ScrollView>
+        {Object.entries(groupedItemsInBasket).map(([key, items]) => (
+          <View key={key}>
+            <Text>{items.length} x</Text>
+            <Text className="flex-1">{items[0]?.name}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
