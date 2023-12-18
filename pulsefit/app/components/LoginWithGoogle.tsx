329889dd1c8bbfb7d298ab5a017dfeec73dfa381
sindabad764 <sindabad764@outlook.com>
2023-12-18 12:29:15 -0600

    pulsefit : google auth working, no connection with mongo

--- pulsefit/app/components/LoginWithGoogle.tsx
+++ pulsefit/app/components/LoginWithGoogle.tsx
@@ -1,42 +1,68 @@
-import React from 'react';
-import { View, Text, Pressable } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button, Image } from 'react-native';
 import { useAuth } from '@realm/react';
-import * as GoogleSignIn from 'expo-google-app-auth';
+import * as Google from 'expo-auth-session/providers/google';
 import colors from '../styles/colors';
 import { AuthOperationName } from '@realm/react';
+import { StatusBar } from 'expo-status-bar';
 
 export const LogInWithGoogle = () => {
-  const { logInWithGoogle, result } = useAuth();
+  const [accessToken, setAccessToken] = useState(null);
+  const [userInfo, setUserInfo] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleGoogleSignIn = async () => {
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId: 'YOUR_ANDROID_CLIENT_ID',
+    iosClientId: '412918610887-k1j69i0v9a3l8o26p11bfded7ncm0qq6.apps.googleusercontent.com',
+    expoClientId: '412918610887-m41emha1f8v7smdmpaf31484b8mubpav.apps.googleusercontent.com',
+  });
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      setAccessToken(response.authentication.accessToken);
+    } else if (response?.type === 'error') {
+      setError(response.error);
+    }
+  }, [response]);
+
+  const getUserData = async () => {
     try {
-      const result = await GoogleSignIn.logInAsync({
-        //androidClientId: 'YOUR_ANDROID_CLIENT_ID',
-        iosClientId: '412918610887-m41emha1f8v7smdmpaf31484b8mubpav.apps.googleusercontent.com', 
-        scopes: ['profile', 'email'],
+      let userInfoResponse = await fetch('https://www.googleapis.com/userinfo/v2/me', {
+        headers: { Authorization: `Bearer ${accessToken}` },
       });
-  
-      if (result.type === 'success') {
-        logInWithGoogle({ idToken: result.idToken });
-      }
+      let userData = await userInfoResponse.json();
+      setUserInfo(userData);
     } catch (error) {
-      console.error('Google sign-in error:', error);
+      console.error('Error fetching user data:', error);
     }
   };
-  
+
+  const showUserData = () => {
+    if (userInfo) {
+      return (
+        <View>
+          <Image source={{ uri: userInfo.picture }} style={{ width: 50, height: 50 }} />
+          <Text>{userInfo.name}</Text>
+          {/* Add more user data fields as needed */}
+        </View>
+      );
+    }
+  };
 
   return (
     <View>
-      <Pressable onPress={handleGoogleSignIn}>
-        <Text>Login with Google</Text>
-      </Pressable>
-
-      {result?.error?.operation === AuthOperationName.LogInWithGoogle && (
-  <Text style={{ color: colors.white }}>
-    Error logging in with Google, please try again
-  </Text>
-)}
-
+      {isLoading && <Text>Loading...</Text>}
+      {error && <Text style={{ color: 'red' }}>{error}</Text>}
+      {showUserData()}
+      <Button
+        title={accessToken ? 'Get User Data' : 'Login'}
+        onPress={() => {
+          setIsLoading(true);
+          accessToken ? getUserData() : promptAsync({ showInRecents: true }).finally(() => setIsLoading(false));
+        }}
+      />
+      <StatusBar style="auto" />
     </View>
   );
 };
