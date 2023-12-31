bd61f04acbc7465df8ec6f92bf25bd86fcde2b5c
sindabad764 <sindabad764@outlook.com>
2023-12-31 17:41:36 -0600

    pulsefit: side menu added

--- pulsefit/app/AppSync.tsx
+++ pulsefit/app/AppSync.tsx
@@ -1,73 +1,51 @@
-// Import React library for building React components
-import React, { useEffect, useState } from 'react';
-// Import Realm React hooks for interacting with Realm, such as useApp, useAuth, useQuery, useRealm, and useUser
-import { useApp, useAuth, useQuery, useRealm, useUser } from '@realm/react';
-// Import React Native components and styles
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { useAuth, useUser } from '@realm/react';
 import { Pressable, StyleSheet, Text } from 'react-native';
-// Import the Task model
 import { Task } from './models/Task';
-// Import the TaskManager and OfflineModeButton components
 import { TaskManager } from './components/TaskManager';
 import { OfflineModeButton } from './components/OfflineModeButton';
-// Import buttonStyles and shadows from the 'styles' folder, and colors from 'styles/colors'
 import { buttonStyles } from './styles/button';
 import { shadows } from './styles/shadows';
 import colors from './styles/colors';
-// Define a functional component named AppSync
+import CustomSidebarMenu from './components/CustomSideMenu';
+
+const Drawer = createDrawerNavigator();
+
+// ... (existing imports)
 
 export const AppSync: React.FC = () => {
-  // Access Realm, user, app, and authentication functions using Realm React hooks
-  const realm = useRealm();
-  const user = useUser();
-  const app = useApp();
   const { logOut } = useAuth();
-
-  // State to manage whether to show completed tasks
-  const [showDone, setShowDone] = useState(false);
-  
-  // Query tasks based on the showDone state
-  const tasks = useQuery(
-    Task,
-    (collection) =>
-      showDone
-        ? collection.sorted('createdAt')
-        : collection.filtered('isComplete == false').sorted('createdAt'),
-    [showDone],
-  );
-
-  // Subscribe to task updates when the component mounts or when tasks change
-  useEffect(() => {
-    realm.subscriptions.update((mutableSubs) => {
-      mutableSubs.add(tasks);
-    });
-  }, [realm, tasks]);
+  const user = useUser();
 
   return (
-    <>
-      {/* Display the app id being used for syncing */}
-      <Text style={styles.idText}>Syncing with app id: {app.id}</Text>
-      {/* Render the TaskManager component to manage and display tasks */}
-      <TaskManager
-        tasks={tasks}
-        userId={user?.id}
-        setShowDone={setShowDone}
-        showDone={showDone}
-      />
-      
-      {/* Pressable button to log out the user */}
-      <Pressable style={styles.authButton} onPress={logOut}>
-      <Text style={styles.authButtonText}>
-    {`Logout ${user.profile.name || user.profile.email}`}
-  </Text>
-      </Pressable>
-
-      {/* Render the OfflineModeButton component */}
-      <OfflineModeButton />
-    </>
+    <NavigationContainer>
+      <Drawer.Navigator
+        drawerContent={(props) => <CustomSidebarMenu {...props} />}
+      >
+        <Drawer.Screen name="Home">
+          {() => (
+            <>
+              
+              <Pressable style={styles.authButton} onPress={logOut}>
+                <Text style={styles.authButtonText}>
+                  {`Logout ${user?.profile.name || user?.profile.email}`}
+                </Text>
+              </Pressable>
+              <OfflineModeButton />
+            </>
+          )}
+        </Drawer.Screen>
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 };
 
-// Define styles for the component using StyleSheet
+// ... (existing styles and export)
+
+
+
 const styles = StyleSheet.create({
   idText: {
     color: '#999',
@@ -82,3 +60,5 @@ const styles = StyleSheet.create({
     ...buttonStyles.text,
   },
 });
+
+export default AppSync;
